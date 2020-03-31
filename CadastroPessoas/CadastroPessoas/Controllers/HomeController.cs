474cb7df84dfdb569f6ef535bf10a8bba840cf92
Gustavo Calegari Silva <gustavo.calegarisilva@hotmail.com>
2020-03-31 20:55:37 -0300

    Add: Estrutura e banco de dados prontos

---
+++ CadastroPessoas/CadastroPessoas/Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using CadastroPessoas.Models;
+using CadastroPessoas.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace CadastroPessoas.Controllers
+{
+    [ApiController]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Pessoa>>> Get([FromServices] DataContext context)
+        {
+            var pessoas = await context.Pessoas.ToListAsync();
+            return pessoas;
+        }
+
+        [HttpGet]
+        [Route("{id: int}")]
+        public async Task<ActionResult<Pessoa>> GetById([FromServices] DataContext context, int id)
+        {
+            var pessoa = await context.Pessoas.Include(x => x.Id)
+                .AsNoTracking()
+                .FirstOrDefaultAsync(x => x.Id == id);
+            return pessoa;
+        }
+
+        [HttpPost]
+        [Route("")]
+        public async Task<ActionResult<Pessoa>> Post(
+            [FromServices] DataContext context,
+            [FromBody] Pessoa model)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Pessoas.Add(model);
+                await context.SaveChangesAsync();
+                return model;
+            } 
+            else
+            {
+                return BadRequest(ModelState);
+            }
+        }
+    }
+}
---
+++ CadastroPessoas/CadastroPessoas/Models/Pessoa.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CadastroPessoas.Models
+{
+    public class Pessoa
+    {
+        [Key]
+        public long Id { get; set; }
+
+        /// <summary>
+        /// CEP
+        /// </summary>
+        [Required]
+        [MinLength(9, ErrorMessage = "O CEP deve conter 9 caracteres")]
+        public string CEP { get; set; }
+
+        /// <summary>
+        /// Cidade
+        /// </summary>
+        [Required]
+        public string Cidade { get; set; }
+
+        /// <summary>
+        /// E-mail
+        /// </summary>
+        [RegularExpression(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$", ErrorMessage = "Digite um e-mail válido")]
+        public string Email { get; set; }
+
+        /// <summary>
+        /// Estado
+        /// </summary>
+        [Required]
+        [MaxLength(2)]
+        public string Estado { get; set; }
+
+        /// <summary>
+        /// Logradouro
+        /// </summary>
+        [Required]
+        public string Logradouro { get; set; }
+
+        /// <summary>
+        /// Nacionalidade
+        /// </summary>
+        [Required]
+        public string Nacionalidade { get; set; }
+
+        /// <summary>
+        /// Nome
+        /// </summary>
+        [Required]
+        [MinLength(2, ErrorMessage = "O nome deve conter pelo menos 2 caracteres")]
+        public string Nome { get; set; }
+
+        /// <summary>
+        /// Sobrenome
+        /// </summary>
+        [Required]
+        [MinLength(2, ErrorMessage = "O sobrenome deve conter pelo menos 2 caracteres")]
+        public string Sobrenome { get; set; }
+
+        /// <summary>
+        /// CPF
+        /// </summary>
+        [Required]
+        [MinLength(14, ErrorMessage = "O CPF deve conter 14 caracteres")]
+        public string CPF { get; set; }
+
+        /// <summary>
+        /// Telefone
+        /// <//summary>
+        [MinLength(9, ErrorMessage = "O telefone deve conter pelo menos 9 caracteres")]
+        public string Telefone { get; set; }
+    }
+}
